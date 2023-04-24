b0ad97193f825501edb61248dbdd0e7f7ba0ee75
css <danielangelim08@gmail.com>
2023-04-24 16:55:53 -0300

    correções da classe triangulo

--- triangulo.py
+++ triangulo.py
@@ -3,32 +3,33 @@ from math import sqrt
 
 class Triangulo:
     def __init__(self, ponto1, ponto2, ponto3):
-        self.po1 = ponto1
-        self.po2 = ponto2
-        self.po3 = ponto3
+        self.p1 = ponto1
+        self.p2 = ponto2
+        self.p3 = ponto3
 
-    def calc_lado(self, A, B):
+    @staticmethod
+    def calc_lado(A, B):
         return Po.calc_dist_entre_pontos(A, B)
     
     def calc_area(self):
-        lado1 = Po.calc_dist_entre_pontos(self.po1, self.po2)
-        lado2 = Po.calc_dist_entre_pontos(self.po2, self.po3)
-        lado3 = Po.calc_dist_entre_pontos(self.po3, self.po1)
+        L1 = Po.calc_dist_entre_pontos(self.p1, self.p2)
+        L2 = Po.calc_dist_entre_pontos(self.p2, self.p3)
+        L3 = Po.calc_dist_entre_pontos(self.p3, self.p1)
 
-        semip = (lado1 + lado2 + lado3)/2
+        semip = (L1 + L2 + L3)/2
 
-        area = sqrt(semip(semip - self.po1)(semip - self.po2)(semip - self.po3))
+        area = sqrt(semip*(semip - L1)*(semip - L2)*(semip - L3))
 
         return area
     
     def verificar(self):
-        lado1 = Po.calc_dist_entre_pontos(self.po1, self.po2)
-        lado2 = Po.calc_dist_entre_pontos(self.po2, self.po3)
-        lado3 = Po.calc_dist_entre_pontos(self.po3, self.po1)
+        L1 = Po.calc_dist_entre_pontos(self.p1, self.p2)
+        L2 = Po.calc_dist_entre_pontos(self.p2, self.p3)
+        L3 = Po.calc_dist_entre_pontos(self.p3, self.p1)
 
-        if (lado1 == lado2) and (lado1 == lado3) :
+        if (L1 == L2) and (L1 == L3) :
             return 'Equilatero'
-        elif (lado1 == lado2) or (lado1 == lado3) or (lado2 == lado3):
+        elif (L1 == L2) or (L1 == L3) or (L2 == L3):
             return 'Isósceles'
         else:
             return 'Escaleno'
